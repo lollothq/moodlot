8a6ddb4d7d58d63474e773d32367bcc04cf8bced
Ta Wei <vistriter@gmail.com>
2020-08-08 13:13:39 +0700

    init project

---
+++ main/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  path: {
+    login: `http://localhost:3000/login`,
+    home: `http://localhost:3000/home`,
+  },
+};
---
+++ main/lib/menu.js
@@ -0,0 +1,102 @@
+const { app, Menu, shell } = require('electron')
+
+const isMac = process.platform === 'darwin'
+
+const tabMenuForApp = [
+  {
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideothers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' },
+    ],
+  },
+]
+
+const macMenuB = [
+  { role: 'pasteAndMatchStyle' },
+  { role: 'delete' },
+  { role: 'selectAll' },
+  { type: 'separator' },
+  {
+    label: 'Speech',
+    submenu: [{ role: 'startspeaking' }, { role: 'stopspeaking' }],
+  },
+]
+
+const macMenuC = [{ type: 'separator' }, { role: 'front' }, { type: 'separator' }, { role: 'window' }]
+
+const template = (logout) => [
+  ...(isMac ? macMenuA : []),
+  {
+    label: 'File',
+    submenu: [isMac ? { role: 'close' } : { role: 'quit' }],
+  },
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? macMenuB : [{ role: 'delete' }, { type: 'separator' }, { role: 'selectAll' }]),
+    ],
+  },
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forcereload' },
+      { role: 'toggledevtools' },
+      { type: 'separator' },
+      { role: 'resetzoom' },
+      { role: 'zoomin' },
+      { role: 'zoomout' },
+      { type: 'separator' },
+      { role: 'togglefullscreen' },
+    ],
+  },
+  // { role: 'windowMenu' }
+  {
+    label: 'Window',
+    submenu: [{ role: 'minimize' }, { role: 'zoom' }, ...(isMac ? macMenuC : [{ role: 'close' }])],
+  },
+  {
+    label: 'Account',
+    submenu: [
+      {
+        label: 'Log out',
+        click: async () => {
+          console.log('logout')
+          logout()
+        },
+      },
+    ],
+  },
+  {
+    role: 'help',
+    submenu: [
+      {
+        label: 'Learn More',
+        click: async () => {
+          await shell.openExternal('https://electronjs.org')
+        },
+      },
+    ],
+  },
+]
+
+module.exports = {
+  initMenu: (logout) => {
+    const menu = Menu.buildFromTemplate(template(logout))
+    Menu.setApplicationMenu(menu)
+  },
+}
